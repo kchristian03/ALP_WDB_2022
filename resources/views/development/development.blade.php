ffd9889f54aeebd14f7062f053d852d60e502c8d
Kevin <kchristian03@student.ciputra.ac.id>
2022-12-20 13:44:04 +0700

    update 20-12-22 13:43

--- resources/views/development/development.blade.php
+++ resources/views/development/development.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{url::asset("/assets.user/css/dev.css")}}">
+    <link rel="stylesheet" href="{{asset("/assets.user/css/dev.css")}}">
     <title>Under Development</title>
 </head>
 <body>
